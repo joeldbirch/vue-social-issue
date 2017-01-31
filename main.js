fabb0580a0b3f5c5c0f72cfa19dc7401603b5e66
Joel Birch <joeldbirch@gmail.com>
2017-01-31 22:24:19 +1100

    Make main.js use ES6 imports.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-window.Vue = require('vue');
-window.VueSocial = require('@blocklevel/vue-social');
+import Vue from 'vue'
+import VueSocial from '@blocklevel/vue-social'
 
 Vue.social.auth = {
   google: 'http://127.0.0.1:8000/auth/google'
 }
+
+Vue.use(VueSocial)
